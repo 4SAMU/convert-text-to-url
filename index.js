4dc646d72704e1ab2238702eade01f483bf9b2fe
4SAMU <samuelnzomo4@gmail.com>
2023-02-17 19:07:55 +0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 export default function makeTextClickable(text) {
   const urlRegex = /(https?:\/\/[^\s]+)/g;
   const phoneRegex = /(\+?\d{1,3}[-.\s]?)?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}/; // Match phone numbers with an optional country code and with spaces, dashes, or dots between digits
-  const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i;
+  const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i; //Match email address
   const parts = text.split(
     /(\b(?:https?:\/\/|mailto:)?\S+\b|\+?\d{1,3}[-.\s]?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b|\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b)/gi
   );
