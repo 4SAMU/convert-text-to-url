a23b2498ba8f02180389a8fc12767deb5195e413
4SAMU <samuelnzomo4@gmail.com>
2023-02-17 18:41:44 +0300

    phone_number, email and href_link checker

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 export default function makeTextClickable(text) {
   const urlRegex = /(https?:\/\/[^\s]+)/g;
-  const phoneRegex = /(\+?\d{1,3}[-.\s]?)?\d{10}([-.\s]?\d{3,4})?/g; // Match phone numbers with an optional country code
-  const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi; // Match email addresses
-  const parts = text.split(/(\b(?:https?:\/\/|mailto:)?\S+\b|\b\d{10}\b)/gi);
+  const phoneRegex = /(\+?\d{1,3}[-.\s]?)?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}/; // Match phone numbers with an optional country code and with spaces, dashes, or dots between digits
+  const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i;
+  const parts = text.split(
+    /(\b(?:https?:\/\/|mailto:)?\S+\b|\+?\d{1,3}[-.\s]?\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b|\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b)/gi
+  );
+
   return parts.map((part, i) => {
     if (urlRegex.test(part)) {
       return (
